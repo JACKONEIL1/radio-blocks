c5106673280411142b9b64be00c7fa74828b476c
JACKONEIL1 <86691354+JACKONEIL1@users.noreply.github.com>
2021-11-17 09:43:12 +1300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    let received_number = 0
+    if (received_number == 1) {
+    	
+    }
+    if (received_number == 2) {
+    	
+    }
+    if (received_number == 3) {
+    	
+    }
+    if (received_number == 4) {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P0, 0)
+})
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendString("left")
+    radio.sendNumber(2)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P0, 180)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    radio.sendString("right")
+    radio.sendNumber(4)
 })
 radio.setGroup(96)
