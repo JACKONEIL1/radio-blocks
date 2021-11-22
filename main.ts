6dbbba8a4f1cd6a2ccbfb366ea3dccb96d8beadb
JACKONEIL1 <86691354+JACKONEIL1@users.noreply.github.com>
2021-11-23 11:22:55 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    let received_number = 0
-    if (received_number == 1) {
-    	
-    }
-    if (received_number == 2) {
-    	
-    }
-    if (received_number == 3) {
-    	
-    }
-    if (received_number == 4) {
-    	
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P0, 0)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(2)
-})
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    pins.servoWritePin(AnalogPin.P1, 120)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P1, 60)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P0, 180)
+	
 })
-input.onGesture(Gesture.TiltRight, function () {
-    radio.sendNumber(4)
-})
-radio.setGroup(96)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+radio.setGroup(69)
